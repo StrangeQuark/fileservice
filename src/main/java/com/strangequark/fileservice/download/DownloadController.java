d94d487d9e1fa1b7fc998ffa6171e55fd5cd8c4d
StrangeQuark <zacharyzirkle@hotmail.com>
2025-02-24 17:37:17 -0600

    Add download Controller/Service

---
+++ src/main/java/com/strangequark/fileservice/download/DownloadController.java
@@ -0,0 +1,19 @@
+package com.strangequark.fileservice.download;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/download")
+public class DownloadController {
+    private final DownloadService downloadService;
+
+    public DownloadController(DownloadService downloadService) {
+        this.downloadService = downloadService;
+    }
+
+    @GetMapping("/{fileName}")
+    public ResponseEntity<?> downloadFile(@PathVariable String fileName) {
+        return downloadService.downloadFile(fileName);
+    }
+}
---
+++ src/main/java/com/strangequark/fileservice/download/DownloadService.java
@@ -0,0 +1,33 @@
+package com.strangequark.fileservice.download;
+
+import com.strangequark.fileservice.error.ErrorResponse;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DownloadService {
+    private final Path uploadDir = Paths.get("uploads");
+
+    public DownloadService() throws IOException {
+        if (!Files.exists(uploadDir)) {
+            Files.createDirectories(uploadDir);
+        }
+    }
+
+    public ResponseEntity<?> downloadFile(String fileName) {
+        try {
+            Path filePath = uploadDir.resolve(fileName);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                    .body(new FileSystemResource(filePath));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return ResponseEntity.status(500).body(new ErrorResponse("File download failed"));
+        }
+    }
+}
