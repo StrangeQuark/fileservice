69066819f3307454bc5d7de4bb985c5e208c2903
StrangeQuark <zacharyzirkle@hotmail.com>
2025-02-28 18:10:19 -0600

    Add endpoint to delete files

--- src/main/java/com/strangequark/fileservice/file/FileService.java
+++ src/main/java/com/strangequark/fileservice/file/FileService.java
@@ -4,6 +4,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,5 +17,11 @@ public class FileService {
         String[] files = folder.list();
 
         return ResponseEntity.ok(files != null ? Arrays.asList(files) : new ArrayList<>());
+    }
+
+    public ResponseEntity<?> deleteFile(String fileName) {
+        File file = new File("uploads/" + fileName);
+
+        return file.delete() ? ResponseEntity.ok("File successfully deleted") : ResponseEntity.status(400).body("File failed to delete");
     }
 }
