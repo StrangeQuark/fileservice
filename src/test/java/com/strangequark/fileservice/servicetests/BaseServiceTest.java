20b6e4a881605e079668035671421b04b5b7bcf9
StrangeQuark <zacharyzirkle@hotmail.com>
2025-03-31 18:47:59 -0500

    Add UploadServiceTest, create BaseServiceTest

---
+++ src/test/java/com/strangequark/fileservice/servicetests/BaseServiceTest.java
@@ -0,0 +1,36 @@
+package com.strangequark.fileservice.servicetests;
+
+import com.strangequark.fileservice.metadata.Metadata;
+import com.strangequark.fileservice.metadata.MetadataRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockMultipartFile;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+public abstract class BaseServiceTest {
+
+    @Autowired
+    public MetadataRepository metadataRepository;
+    public MockMultipartFile mockMultipartFile;
+    private final Path uploadDir = Paths.get("uploads");
+
+    @BeforeEach
+    void setup() {
+        mockMultipartFile = new MockMultipartFile("testFile", "testFile.txt",
+            "text/plain", "Test file data".getBytes());
+    }
+
+    @AfterEach
+    void teardown() {
+        Optional<Metadata> metadata = metadataRepository.findById_FileNameAndId_Username("testFile.txt", "testUser");
+
+        File file = uploadDir.resolve(metadata.get().getFileUUID()).toFile();
+        file.delete();
+        metadataRepository.delete(metadata.get());
+    }
+}
