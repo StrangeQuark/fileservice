1d9e7380c8e65cf8da2290fe439cd772a47ff7ef
StrangeQuark <zacharyzirkle@hotmail.com>
2025-07-18 19:10:40 -0500

    Clean EncryptionService class to make it more readable

--- src/main/java/com/strangequark/fileservice/utility/EncryptionService.java
+++ src/main/java/com/strangequark/fileservice/utility/EncryptionService.java
@@ -1,12 +1,12 @@
 package com.strangequark.fileservice.utility;
 
-import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Base64;
 
@@ -14,46 +14,41 @@ import java.util.Base64;
 public class EncryptionService {
     private static final Logger LOGGER = LoggerFactory.getLogger(EncryptionService.class);
 
-    private final String secretKey;
+    private final String ALGORITHM = "AES";
 
-    private SecretKeySpec keySpec;
-
-    public EncryptionService(@Value("${encryption.key}") String secretKey) {
-        this.secretKey = secretKey;
-    }
-
-    @PostConstruct
-    public void init() {
-        keySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
-    }
+    @Value("${ENCRYPTION_KEY}")
+    private String ENCRYPTION_KEY;
 
     public String encrypt(String data) {
         try {
             LOGGER.info("Attempting to encrypt data");
 
-            Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+            SecretKey key = new SecretKeySpec(ENCRYPTION_KEY.getBytes(), ALGORITHM);
+
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
 
             LOGGER.info("Data successfully encrypted");
             return Base64.getEncoder().encodeToString(cipher.doFinal(data.getBytes()));
-        } catch (Exception ex) {
+        } catch (Exception e) {
             LOGGER.error("Encryption error");
-            throw new RuntimeException("Encryption error", ex);
+            throw new RuntimeException("Encryption error", e);
         }
     }
 
     public String decrypt(String data) {
         try {
             LOGGER.info("Attempting to decrypt data");
+            SecretKey key = new SecretKeySpec(ENCRYPTION_KEY.getBytes(), ALGORITHM);
 
             Cipher cipher = Cipher.getInstance("AES");
-            cipher.init(Cipher.DECRYPT_MODE, keySpec);
+            cipher.init(Cipher.DECRYPT_MODE, key);
 
             LOGGER.info("Data successfully decrypted");
             return new String(cipher.doFinal(Base64.getDecoder().decode(data)));
-        } catch (Exception ex) {
+        } catch (Exception e) {
             LOGGER.error("Decryption error");
-            throw new RuntimeException("Decryption error", ex);
+            throw new RuntimeException("Decryption error", e);
         }
     }
 }
